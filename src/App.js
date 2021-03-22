2a997313970e2e2eeb76ab16c5ec915f3554f09f
jatin mishra <jatinmishra@jatins-MacBook-Pro.local>
2021-03-22 17:49:35 -0600

    First Commit

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import './App.css';
+
+
+const App = () => {
+    return(
+        <>
+        <div className="one">
+           <ul className="two">
+               <li>Home</li>
+               <li>About</li>
+               <li>Contact</li>
+               <li>Services</li>
+               </ul> 
+        </div>
+        </>
+    )
+}
+
+export default App;
